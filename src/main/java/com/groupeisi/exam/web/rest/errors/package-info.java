4343e6a3faee999cbe2326a0302f975204f64f8c
coumba <coumbissthiam96@gmail.com>
2024-04-03 18:47:09 +0000

    Initial version of exam1 generated by generator-jhipster@8.1.0

---
+++ src/main/java/com/groupeisi/exam/web/rest/errors/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer error handling.
+ */
+package com.groupeisi.exam.web.rest.errors;
---
+++ src/test/java/com/groupeisi/exam/security/jwt/AuthenticationIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.groupeisi.exam.security.jwt;
+
+import com.groupeisi.exam.config.SecurityConfiguration;
+import com.groupeisi.exam.config.SecurityJwtConfiguration;
+import com.groupeisi.exam.config.WebConfigurer;
+import com.groupeisi.exam.management.SecurityMetersService;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import tech.jhipster.config.JHipsterProperties;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = {
+        JHipsterProperties.class,
+        WebConfigurer.class,
+        SecurityConfiguration.class,
+        SecurityJwtConfiguration.class,
+        SecurityMetersService.class,
+        JwtAuthenticationTestUtils.class,
+    }
+)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface AuthenticationIntegrationTest {
+}
